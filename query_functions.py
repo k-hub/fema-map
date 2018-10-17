ba0a70cf95207f9c54a8868ccd0df53be186329a
Kathy <kathyy.ma@gmail.com>
2018-10-17 14:26:50 -0700

    Add query function to query for disasters by a given year to return the count of counties affected, state, abbreviation, and disaster type

--- query_functions.py
+++ query_functions.py
@@ -1,5 +1,5 @@
-from models import Fema
-from sqlalchemy import extract
+from models import Fema, State
+from sqlalchemy import func
 
 
 def get_fema_data(year=None, disaster_type=None, order=None):
@@ -32,7 +32,7 @@ def get_fema_data(year=None, disaster_type=None, order=None):
             - Tsunami
             - Typhoon
             - Volcano
-    - 'order': order results by date in ascending or descending order.
+    - 'order': order results by state in ascending or descending order.
         Example:
             - asc
             - desc
@@ -40,29 +40,53 @@ def get_fema_data(year=None, disaster_type=None, order=None):
 
     result = None
     base_query = Fema.query.with_entities(
-                Fema.incident_begin_date,
+                Fema.fy_declared,
                 Fema.state, Fema.incident_type)
 
-    # Handle ordering by ascending or descending incident_begin_date.
-    if order == 'asc':
-        base_query = base_query.order_by(
-                    Fema.incident_begin_date.asc(), Fema.state.asc())
+    # Handle ordering by ascending or descending state name.
+    if order == 'desc':
+        base_query = base_query.order_by(Fema.state.desc())
     else:
-        base_query = base_query.order_by(
-                    Fema.incident_begin_date.desc(), Fema.state.asc())
+        base_query = base_query.order_by(Fema.state.asc())
 
     # Handle filtering by year and/or disaster_type.
     if year and disaster_type:
         result = base_query.filter(
-                extract('year', Fema.incident_begin_date) == year,
+                Fema.fy_declared == year,
                 Fema.incident_type == disaster_type)
     elif year:
-        result = base_query.filter(
-                extract('year', Fema.incident_begin_date) == year)
+        result = base_query.filter(Fema.fy_declared == year)
     elif disaster_type:
         result = base_query.filter(Fema.incident_type == disaster_type)
     # No filters have been applied so just return.
     elif not year or not disaster_type:
         return base_query
+
+    return result
+
+
+def get_joined_fema_data(year, order=None):
+    result = None
+    base_query = Fema.query.join(State).with_entities(
+                Fema.state,
+                func.max(State.state),
+                func.max(Fema.fy_declared),
+                func.max(Fema.incident_type),
+                func.count(Fema.state)).group_by(Fema.state)
+
+    # Handle ordering by ascending/descending state name
+    # or ascending/descending disaster type.
+    if order == 'state_desc':
+        base_query = base_query.order_by(Fema.state.desc())
+    elif order == 'disaster_desc':
+        base_query = base_query.order_by(Fema.incident_type.desc())
+    elif order == 'disaster_asc':
+        base_query = base_query.order_by(Fema.incident_type.asc())
+    else:
+        base_query = base_query.order_by(Fema.state.asc())
+
+    # Handle filtering by year.
+    result = base_query.filter(
+            Fema.fy_declared == year)
 
     return result
